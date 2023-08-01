662ea4996f879a589f40a5abc7b347d9ef720e6f
taka4488 <138207123+taka4488@users.noreply.github.com>
2023-08-02 00:12:47 +0900

    Add files via upload
    js v_2

--- kadai_009/holiday.js
+++ kadai_009/holiday.js
@@ -3,3 +3,12 @@ const holidays = ["正月","成人の日","建国記念日","天皇誕生日","�
 for (let i = 0; i <= 15; i++ ) {
   console.log(holidays[i]);
 }
+
+let index = 0;
+
+while (index < holidays.length) {
+
+  console.log(holidays[index]);
+
+  index++;
+}
